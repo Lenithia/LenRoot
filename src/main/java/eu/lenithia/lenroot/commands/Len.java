ea940abc260c86e5f1d7de2c3a7ef33bceb0fa75
Len <len.trough843@passinbox.com>
2024-06-09 01:49:06 +0200

    Len command now actually does something, but sometimes does errors,

--- src/main/java/eu/lenithia/lenroot/commands/Len.java
+++ src/main/java/eu/lenithia/lenroot/commands/Len.java
@@ -12,7 +12,10 @@ import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.sql.Time;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class Len implements TabExecutor {
 
@@ -39,6 +42,7 @@ public class Len implements TabExecutor {
 
      */
 
+    private static HashMap<UUID, Time> confirm = new HashMap<>();
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
@@ -46,6 +50,42 @@ public class Len implements TabExecutor {
         if (args.length == 0) {
             commandSender.sendMessage(plInfo());
             return true;
+        } else {
+            switch (args[0]) {
+                case "help":
+                    commandSender.sendMessage(help());
+                    break;
+                case "load":
+                    if (args.length == 2) {
+                        load(commandSender, args[1]);
+                    } else {
+                        commandSender.sendMessage("<red> Invalid usage. Use /len load <module>");
+                    }
+                    break;
+                case "unload":
+                    if (args.length == 2) {
+                        unload(commandSender, args[1]);
+                    } else {
+                        commandSender.sendMessage("<red> Invalid usage. Use /len unload <module>");
+                    }
+                    break;
+                case "reload":
+                    if (args.length == 2) {
+                        reload(commandSender, args[1]);
+                    } else {
+                        commandSender.sendMessage("<red> Invalid usage. Use /len reload <module>");
+                    }
+                    break;
+
+                // TODO: Implement these commands (register & unregister)
+                case "register":
+
+                case "unregister":
+
+                default:
+                    commandSender.sendMessage("<red> Unknown command. Use /len help for help.");
+                    break;
+            }
         }
 
 
@@ -55,14 +95,6 @@ public class Len implements TabExecutor {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         return List.of();
-    }
-
-    private void load(CommandSender sender ,String name) {
-        LenFeature feature = lenRoot.getLenFeatureAPI().getLenFeature(name);
-        if (feature != null) {
-            lenRoot.getLenFeatureAPI().load(feature);
-            sender.sendMessage("LenFeature " + name + " was loaded!");
-        }
     }
 
     private Component plInfo() {
@@ -75,10 +107,10 @@ public class Len implements TabExecutor {
         String pluginVersion = pluginMeta.getVersion();
 
         TextComponent.Builder component = Component.text()
-                .append(MessageUtils.deserialize("<gradient:#ea76f5:#ed9ff5><strikethrough>                                                                                 "))
+                .append(MessageUtils.deserialize("<gradient:#ea76f5:#ed9ff5><strikethrough>                                                                                "))
                 .appendNewline()
                 .appendNewline()
-                .append(MessageUtils.deserialize("   <#ea76f5>" + pluginName ))
+                .append(MessageUtils.deserialize("<#ea76f5>   " + pluginName ))
                 .appendNewline()
                 .appendNewline()
                 .append(MessageUtils.deserialize("<#ea76f5>   Description: <#ffffff>" + pluginDescription ))
@@ -88,13 +120,61 @@ public class Len implements TabExecutor {
                 .append(MessageUtils.deserialize("<#ea76f5>   Version: <#ffffff>" + pluginVersion ))
                 .appendNewline()
                 .appendNewline()
-                .append(MessageUtils.deserialize("<#ea76f5>   Help command: <#ffffff><hover:show_text:'<#ea76f5>click to execute!'><click:run_command:len help>/len help</click></hover>" ))
+                .append(MessageUtils.deserialize("<#ea76f5>   Help command: <#ffffff><hover:show_text:'<#ea76f5>Click to execute!'><click:suggest_command:len help>/len help</click></hover>" ))
                 .appendNewline()
-                .appendNewline()
-                .append(MessageUtils.deserialize("<gradient:#ea76f5:#ed9ff5><strikethrough>                                                                                 "))
+                .append(MessageUtils.deserialize("<gradient:#ea76f5:#ed9ff5><strikethrough>                                                                                "))
                 ;
         return component.build();
     }
 
+    private Component help() {
+        TextComponent.Builder component = Component.text()
+                .append(MessageUtils.deserialize("<gradient:#ea76f5:#ed9ff5><strikethrough>                                                                                "))
+                .appendNewline()
+                .append(MessageUtils.deserialize("<#ea76f5>   /len help: <#ffffff> Shows this help message."))
+                .appendNewline()
+                .append(MessageUtils.deserialize("<#ea76f5>   /len load <module>: <#ffffff> Loads a module."))
+                .appendNewline()
+                .append(MessageUtils.deserialize("<#ea76f5>   /len unload <module>: <#ffffff> Unloads a module."))
+                .appendNewline()
+                .append(MessageUtils.deserialize("<#ea76f5>   /len reload <module>: <#ffffff> Reloads a module."))
+                .appendNewline()
+                .append(MessageUtils.deserialize("<gradient:#ea76f5:#ed9ff5><strikethrough>                                                                                "))
+                ;
+        return component.build();
+    }
+
+
+    // TODO: Implement confirmation system for loading/unloading/reloading features
+    private boolean conformation(CommandSender sender) {
+        return true;
+    }
+
+
+
+
+    private void load(CommandSender sender ,String name) {
+        LenFeature feature = lenRoot.getLenFeatureAPI().getLenFeature(name);
+        if (feature != null) {
+            lenRoot.getLenFeatureAPI().load(feature);
+            sender.sendMessage(MessageUtils.deserialize("<#ffffff> LenFeature <#ea76f5>" + name + "<#ffffff> was loaded!"));
+        }
+    }
+
+    private void unload(CommandSender sender, String name) {
+        LenFeature feature = lenRoot.getLenFeatureAPI().getLenFeature(name);
+        if (feature != null) {
+            lenRoot.getLenFeatureAPI().unload(feature);
+            sender.sendMessage(MessageUtils.deserialize("<#ffffff> LenFeature <#ea76f5>" + name + "<#ffffff> was unloaded!"));
+        }
+    }
+
+    private void reload(CommandSender sender, String name) {
+        LenFeature feature = lenRoot.getLenFeatureAPI().getLenFeature(name);
+        if (feature != null) {
+            feature.reload();
+            sender.sendMessage(MessageUtils.deserialize("<#ffffff> LenFeature <#ea76f5>" + name + "<#ffffff> was reloaded!"));
+        }
+    }
 
 }
